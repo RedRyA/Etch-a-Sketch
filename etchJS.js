fed0a5d65068bb5c721c8e28a812d650d3764eb0
RedRyA <65385633+RedRyA@users.noreply.github.com>
2022-07-07 02:02:27 -0500

    Add files via upload
    The final changes are made.

--- etchJS.js
+++ etchJS.js
@@ -1,5 +1,5 @@
 
-// GLOBAL VARIABLES THAT I SHOULD PROBABLY CAPITALIZE
+// GLOBAL VARIABLES //
 var container = document.querySelector(".container")
 var rainbow = document.querySelector("#rainbow")
 var clearScreen = document.querySelector(".clearScreen")
@@ -8,36 +8,30 @@ var slider = document.querySelector(".slider")
 var squaresSelect= document.querySelectorAll(".squareSelects")
 var colorChoices = document.querySelectorAll(".color-choice")
 var glow=document.querySelectorAll(".glow")
-
 var color="black"
 
 // Creates a grid based slider input//
  function createGrid(size){
-let totalSize=size*size
+  let totalSize=size*size
 
-for (let i = 1; i<=totalSize; i++){
-  const square=document.createElement('div')
-container.style.gridTemplateColumns=`repeat(${size},1fr)`
-container.style.gridTemplateRows=`repeat(${size},1fr)`
+  for (let i = 1; i<=totalSize; i++){
+    const square=document.createElement('div')
+    container.style.gridTemplateColumns=`repeat(${size},1fr)`
+    container.style.gridTemplateRows=`repeat(${size},1fr)`
+    container.appendChild(square)
 
-
-container.appendChild(square)
-
-var glow=square.className=("glow")
-
-square.addEventListener("mouseover",mouseOver)
-
+    square.addEventListener("mouseover",mouseOver)
 
 }
  }
-// Default size
- createGrid(16)
 
+// Default size//
+ createGrid(16)
+// Function to adjust the size of divs via the slider //
 function pixelSizer(){
 let gridPixels = container.querySelectorAll("div")
 gridPixels.forEach (gridPixel => gridPixel.remove());
 createGrid(slider.value)
-
 }
 
 // allows the coloring of the divs in the main container
@@ -51,21 +45,19 @@ function mouseOver() {
   this.style.backgroundColor=color
   }
 }
+
 // changes color based on button clicked 
 function changeColor(choice){
 
    color=choice 
    mouseOver()
-
 }
 
-
 // Clears main container
-function clearAll(){
+  function clearAll(){
   var clearIt=document.querySelectorAll(".glow")
-for(i in clearIt){
+  for(i in clearIt){
   console.log(clearIt[i])
-  
   clearIt[i].style.backgroundColor="white"
 }
 }
---
+++ toDo.js
@@ -0,0 +1,36 @@
+/* toDo in HTML */
+
+
+//////////////////////////////
+
+/* toDo in CSS */
+
+///////////////////////////////
+
+/* toDo in Javascript */
+
+
+/*//////////////////////////////*/
+
+/*  Done HTML */
+// DONE!! add a clear button
+//DONE!! add a slider for pixel size
+//DONE!! add shader
+
+/*////////////////////////////////*/
+/* Done CSS */
+//DONE!! arrange buttons
+//DONE  add background color to main screen
+
+/*////////////////////////////*/
+/*Done JS */
+///// DONE!! Set Event Target to change colors on button click?????
+// DONE !!! make newDiv invisible !!!
+// DONE!! add a clear color function
+// DONE!! Fit newDiv to div "container"
+//DONE!! align container as a square
+// dynamically resize container to fit content
+//Done!! add a color picker function
+// DONE!! add a rainbow color funtion
+//DONE!! User selects size of etch a sketch function
+
